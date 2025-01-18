cf9f8ffbcb0adf9ebc31234dc386e0539f88338d
hadleysg <hadleygarff@gmail.com>
2025-01-17 22:23:27 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Console.WriteLine("Welcome to the dice throwing simulator!");
+
+        Console.WriteLine("How many dice rolls would you like to simulate?");
+
+        string numRolls = Console.ReadLine();
+        
+        float percentRools = 
+        
+        
+       
+
+
+
+    }
+
+
+}
---
+++ Roll.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission_2
+{
+    public class Roll
+    {
+        public int[] RollDice(int numRolls) 
+        {
+            Random random = new Random();
+
+            // This sets up an array of integers
+             int[] numCount = new int[13];
+
+            // this will loop until the amount of rolls the user input is complete
+            for (int icount = 0; icount < numRolls; icount++)
+            {
+                // Simulate rolling two 6-sided dice
+                int die1 = random.Next(1, 7); // Random number between 1 and 6
+                int die2 = random.Next(1, 7); // Random number between 1 and 6
+                int sum = die1 + die2;
+
+                //this will add the sum to the numCount array
+                numCount[sum]++;
+
+            }
+
+            // this will return the array with the each number that was rolled
+            return numCount;
+        }
+    }
+}
